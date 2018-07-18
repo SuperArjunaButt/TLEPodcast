1b32bfe87610ed394a293c881e1bc8ef3db2327a
Arjuna Gonzales <34360172+SuperArjunaButt@users.noreply.github.com>
2018-07-17 21:49:23 -0400

    Initial commit

---
+++ FPClicker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPClicker : MonoBehaviour {
+
+	private Camera cam;
+	private RaycastHit hit;
+	private ClickableItem clickedItem;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+		clickedItem = null;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(new Vector3((cam.pixelWidth-1)/2, (cam.pixelHeight-1)/2, 0));
+			GameObject itm = null;
+
+			if(Physics.Raycast(ray, out hit, 5000.0f)) {
+				itm = hit.collider.gameObject;
+				Debug.DrawRay(cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f)), itm.transform.position, Color.green, 3.0f, false);
+				//Debug.DrawRay(ray, itm.transform.position, Color.green, 3.0f, false);
+				clickedItem = itm.GetComponentInParent<ClickableItem>();
+				if(clickedItem != null) {
+					Debug.Log("Hit a clickable item");
+					clickedItem.OnPlayerClick(hit.collider.gameObject.name);
+				}
+				
+			}
+		}
+	}
+}
---
+++ PooperSoundController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PooperSoundController : ClickableItem {
+
+	[SerializeField] private AudioSource soundSource;
+	[SerializeField] private Animator anim;
+	
+
+	enum PlayerState {STOPPED, PLAYING, PAUSED, REWINDING, FASTFORWARDING};
+	private PlayerState pstate;
+
+	// Use this for initialization
+	void Start () {
+		pstate = PlayerState.STOPPED;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void OnPlayerClick(string objHit) {
+		//Debug.Log("In OnPlayerClick");
+		if(objHit.Contains("Handle")) {
+			//Debug.Log("Clicked Handle");
+			anim.SetTrigger("PlayTrigger");
+		}
+		else if(objHit.Contains("Seat")) {
+			Debug.Log("Clicked Seat or SeatLid");
+			string currentState = (anim.GetCurrentAnimatorClipInfo(0))[0].clip.name;
+			//Debug.Log(currentState);
+			if(currentState == "Idle") {
+				anim.SetTrigger("StartRewindTrigger");
+				Rewind(true);
+			}
+			else if (currentState == "LidLoweredIdle") {
+				anim.SetTrigger("StopRewindTrigger");
+				Rewind(false);
+			}
+
+		}
+		else {
+			Debug.Log("Huh?  Hit a " + objHit);
+		}
+	}
+
+	void Play() {
+		soundSource.Play();
+		pstate = PlayerState.PLAYING;
+	}
+
+	void Pause() {
+		//soundSource.Pause();
+		//pstate = PlayerState.PAUSED;
+	}
+
+	void Rewind(bool DoOrDont) {
+		//pstate = PlayerState.REWINDING;
+		if(DoOrDont) {
+			Debug.Log("Rewinding");
+			soundSource.Pause();
+			soundSource.timeSamples = audio.clip.samples - 1;
+ 			soundSource.pitch = -1;
+ 			soundSource.Play();
+		}
+		else {
+			Debug.Log("Stopping Rewind");
+		}
+	}
+
+	void FastForward() {
+		//pstate = PlayerState.FASTFORWARDING;
+	}
+
+
+}
